48d619a27aa278a2e92fbbd73acc063433322d66
khomyakova_l <khomyakova_l@lwo.by>
2020-11-03 16:34:38 +0300

    add intrpolation/binding in product-card

--- 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
+++ 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
@@ -1,7 +1,11 @@
 import {
   Component,
   ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter,
 } from '@angular/core';
+import { IProduct } from '../../../../../shared/mocks/1-components/product';
 
 @Component({
   selector: 'ngx-shop-content-product',
@@ -9,6 +13,16 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryProductComponent {
+  @Input() public product: IProduct = {} as IProduct;
+  @Output() public addToCart = new EventEmitter();
+  @Output() public goToProduct = new EventEmitter();
 
+  addToBasket(): void {
+    this.addToCart.emit();
+  }
+
+  redirectTo(): void {
+    this.goToProduct.emit();
+  }
 
 }
