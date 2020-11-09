1bc7c3393a125b9063ae9b204fb41b33507342eb
Liudmila Khomyakova <LV.Khomyakova@gmail.com>
2020-11-09 20:09:32 +0300

    add interpolation/binding in suggested-product

--- 1-components/4-suggested-product/src/app/product-card/product-card.component.ts
+++ 1-components/4-suggested-product/src/app/product-card/product-card.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { IProduct } from '../../../../../shared/mocks/1-components/product';
 
 @Component({
   selector: 'ngx-shop-product-card',
   templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent {
+  @Input() public product: IProduct = {} as IProduct;
+  @Output() public goToProduct = new EventEmitter();
 
+  redirectTo(): void {
+    this.goToProduct.emit();
+  }
 }
