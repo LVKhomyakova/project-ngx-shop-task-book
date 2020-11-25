f4b533fae81234219a60c7675d193ea0450f1a4b
Liudmila Khomyakova <LV.Khomyakova@gmail.com>
2020-11-25 15:39:02 +0300

    add directives in categories

--- 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
+++ 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ICategory } from '../../../../../shared/mocks/3-directives/categories';
 
 @Component({
   selector: 'ngx-shop-side-menu',
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+  @Input() categories: ICategory[] = [];
+  @Output() categoryChange: EventEmitter<string> = new EventEmitter();
+  currentName: string | null = null;
+
+  redirectTo(subCategoryName: string): void {
+    this.categoryChange.emit(subCategoryName);
+  }
+
+  hover(categoryName: string): void {
+    this.currentName = categoryName;
+  }
+
+  mouseLeave(): void {
+    this.currentName = null;
+  }
+}
