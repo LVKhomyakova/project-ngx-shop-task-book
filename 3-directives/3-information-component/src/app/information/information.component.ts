50d3c0ff32d2ccce098ab7cc70d332bf76a43989
Liudmila Khomyakova <LV.Khomyakova@gmail.com>
2020-11-25 18:12:33 +0300

    add directives in information

--- 3-directives/3-information-component/src/app/information/information.component.ts
+++ 3-directives/3-information-component/src/app/information/information.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { IProduct } from '../../../../../shared/mocks/3-directives/product-information';
 
 @Component({
   selector: 'ngx-shop-information',
   templateUrl: './information.component.html',
 })
-export class InformationComponent {}
+export class InformationComponent {
+  @Input() product?: IProduct = null;
+  @Output() addToCart: EventEmitter<string> = new EventEmitter<string>();
+  isShow = false;
+
+  addToBasket(productName: string) {
+    this.addToCart.emit(`${productName} - добавлен в корзину`);
+  }
+
+  show(): void {
+    this.isShow = !this.isShow;
+  }
+
+}
