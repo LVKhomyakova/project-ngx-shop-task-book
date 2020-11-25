f70b59e4123e0fe236d66144873d373100b7f6d0
Liudmila Khomyakova <LV.Khomyakova@gmail.com>
2020-11-25 21:42:17 +0300

    add directives in category

--- 3-directives/5-category-dropdown-component/src/app/category-dropdown/category-dropdown.component.ts
+++ 3-directives/5-category-dropdown-component/src/app/category-dropdown/category-dropdown.component.ts
@@ -1,7 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ICategory } from '../../../../../shared/mocks/3-directives/categories';
 
 @Component({
   selector: 'ngx-shop-category-dropdown',
   templateUrl: './category-dropdown.component.html',
 })
-export class CategoryDropdownComponent {}
+export class CategoryDropdownComponent {
+  @Input() categories: ICategory[] = [] as ICategory[];
+  @Output() subCategorySelectEvent: EventEmitter<string> = new EventEmitter<string>();
+  currentIndex: number | null = null;
+  currentCategory = '';
+
+  subCategorySelect(subCategoryName: string): void{
+    this.subCategorySelectEvent.emit(subCategoryName);
+  }
+
+  showSubCategories(categoryIndex: number): void {
+    this.currentIndex = categoryIndex;
+  }
+
+  collapse(categoryIndex: number): boolean {
+    return  this.currentIndex !== categoryIndex;
+  }
+
+}
