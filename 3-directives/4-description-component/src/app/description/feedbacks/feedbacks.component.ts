6c11132b1d2a2d158215d461fff6ca544c4a77be
Liudmila Khomyakova <LV.Khomyakova@gmail.com>
2020-11-25 19:55:50 +0300

    add directives in description

--- 3-directives/4-description-component/src/app/description/feedbacks/feedbacks.component.ts
+++ 3-directives/4-description-component/src/app/description/feedbacks/feedbacks.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { IFeedback } from '../../../../../../shared/mocks/3-directives/product-information';
 
 @Component({
   selector: 'ngx-shop-feedbacks',
   templateUrl: './feedbacks.component.html',
   styleUrls: ['./feedbacks.component.sass'],
 })
-export class FeedbacksComponent {}
+export class FeedbacksComponent {
+  @Input() feedbacks: IFeedback[] = [] as IFeedback[];
+  @Output() addFeedbackEvent: EventEmitter<string> = new EventEmitter<string>();
+
+  addNewFeedback(): void {
+    this.addFeedbackEvent.emit('Отзыв добавлен');
+  }
+}
