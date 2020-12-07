28f62563eef37bb60c58c7590b0d0781c516f0c1
Liudmila Khomyakova <LV.Khomyakova@gmail.com>
2020-12-07 18:06:19 +0300

    add forms in order-form-component

--- 5-forms/1-order-form-component/src/app/order-form/order-form.component.ts
+++ 5-forms/1-order-form-component/src/app/order-form/order-form.component.ts
@@ -1,8 +1,51 @@
-import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ValidationErrors, FormControl } from '@angular/forms';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ngx-shop-order-form',
   templateUrl: './order-form.component.html',
   styleUrls: ['./order-form.component.sass'],
 })
-export class OrderFormComponent {}
+export class OrderFormComponent implements OnInit {
+  @Output() confirm: EventEmitter<FormData> = new EventEmitter<FormData>();
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: ['Balu', [Validators.required]],
+      telephone: ['+375 44 1234567', [Validators.required, this.phoneValidator]],
+      email: ['q@w.e', [Validators.email, Validators.required]],
+      address: ['vklfvjkv', [Validators.required]],
+      });
+  }
+
+  submit(): void {
+    if (this.form.valid) {
+      this.confirm.emit(this.form.value);
+      this.form.reset();
+    } else {
+      this.form.setErrors({
+        email: this.form.get('email')?.errors,
+        telephone: this.form.get('telephone')?.errors,
+        name: this.form.get('name')?.errors,
+      });
+    }
+  }
+
+  phoneValidator(phoneControl: FormControl): ValidationErrors | null {
+    const phone = phoneControl.value;
+    return phone && phone.substring(0, 1) === '+' && phone.length >= 13
+    ? null
+    : {
+        isNotMatch: true,
+        error: 'Телефон должен быть в формате +44 7911 123456',
+      };
+
+
+
+  }
+}
